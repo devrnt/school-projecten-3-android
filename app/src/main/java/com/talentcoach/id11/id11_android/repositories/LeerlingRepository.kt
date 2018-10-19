e02251f55c4c020e324ff83c2614c6d9803b1ab1
SamDhondt <sam.dhondt.w1599@student.hogent.be>
2018-10-19 13:09:06 +0200

    Toevoegen http GET request in getLeerlingById methode in LeerlingRepository

--- app/src/main/java/com/talentcoach/id11/id11_android/repositories/LeerlingRepository.kt
+++ app/src/main/java/com/talentcoach/id11/id11_android/repositories/LeerlingRepository.kt
@@ -1,12 +1,33 @@
 package com.talentcoach.id11.id11_android.repositories
 
+import android.app.Activity
+import android.content.Context
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonRequest
+import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Klaxon
 import com.talentcoach.id11.id11_android.models.Leerling
 
 class LeerlingRepository {
-    private val leerling: Leerling = Leerling(listOf("teamwork"))
+    private var leerling: Leerling? = null
 
-    fun getLeerlingById(id: Long): Leerling{
+    fun getLeerlingById(id: Long): Leerling? {
+        val queue = Volley.newRequestQueue(ApplicationContextProvider().getContext())
+        val url = "https://localhost:5001/api/leerlingen"
+
+// Request a string response from the provided URL.
+        val req = StringRequest(Request.Method.GET, url + id.toString(),
+                Response.Listener<String> { response ->
+                    leerling = Klaxon().parse<Leerling>(response)
+                },
+                Response.ErrorListener { })
+
+// Add the request to the RequestQueue.
+        queue.add(req)
         // TODO: http GET
         return leerling
     }
