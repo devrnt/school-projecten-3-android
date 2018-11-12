01c6344c6a6acdeb2ff72a9cdff3129a4a75488b
Jonas De Vrient <jonas.devrient.y0029@student.hogent.be>
2018-11-12 19:50:40 +0100

    Add class to run all tests

---
+++ app/src/androidTest/java/com/talentcoach/id11/id11_android/AllTestsSuite.kt
@@ -0,0 +1,19 @@
+package com.talentcoach.id11.id11_android
+
+import com.talentcoach.id11.id11_android.login.LoginActivityTest
+import com.talentcoach.id11.id11_android.profiel.ProfielActivityTest
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+
+/**
+ * Runs all test cases
+ */
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+        InfoActivityTestSuite::class,
+        WerkaanbiedingActivityTestSuite::class,
+        ProfielActivityTest::class,
+        LoginActivityTest::class
+)
+class AllTestsSuite
