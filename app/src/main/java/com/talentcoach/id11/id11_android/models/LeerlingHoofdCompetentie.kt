0357a0e3583f09768dc0c4eb93b1599894769bea
Ocean97Li <ocean97li@gmail.com>
2018-12-16 19:52:28 +0100

    added model documentation part 2

--- app/src/main/java/com/talentcoach/id11/id11_android/models/LeerlingHoofdCompetentie.kt
+++ app/src/main/java/com/talentcoach/id11/id11_android/models/LeerlingHoofdCompetentie.kt
@@ -3,7 +3,19 @@ package com.talentcoach.id11.id11_android.models
 import com.google.gson.annotations.SerializedName
 import java.util.*
 import kotlin.collections.ArrayList
-
+/**
+ * Deel van *models*.
+ *
+ * LeerlingHoofdCompetentie het verband tussen een leerling en een hoofdcompentie,
+ * gekent door de leerling, bevat de hoofdcompetentie, veld geslaagd, leerlingdeelcompetenties, ...
+ * een verwijzing naar de competentie en een veld geslaagd en de datum van geslaag
+ * @property id het id van de LeerlingHoofdCompetentie
+ * @property behaald geeft aan of de Hoofdcompentie al behaald is of niet (behaal als alle deelcompetenties behaald zijn)
+ * @property datumGeslaagd indien behaald geeft deze aan wanneer dit gebeurde
+ * @property hoofdCompetentie het hoofcompetentie object met de omschrijving
+ * @property deelCompetenties bevat de leerlingdeelcompenties die bij deze hoofdcompetentie horen
+ * @constructor Maakt een nieuw LeerlinHoofdCompetentie object
+ */
 class LeerlingHoofdCompetentie(
         val id:Int,
         val behaald: Boolean,
--- app/src/main/java/com/talentcoach/id11/id11_android/models/SpecifiekeInfo.kt
+++ app/src/main/java/com/talentcoach/id11/id11_android/models/SpecifiekeInfo.kt
@@ -1,3 +1,12 @@
 package com.talentcoach.id11.id11_android.models
-
+/**
+ * Deel van *models*.
+ *
+ * Specifieke Info => Nog niet uitgerwerkt, niet in de applicatie
+ * @property id het id van de specifieke info
+ * @property titel de titel van de specifieke info
+ * @property omschrijving de omschrijving van de specifieke info
+ * @property werkgever de bijhorende werkgever van wie de info komt
+ * @constructor Maakt een nieuw SpecifiekeInfo object
+ */
 class SpecifiekeInfo(val id: Int, val titel: String, val omschrijving: String, val werkgever: Werkgever)
